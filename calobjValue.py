2a242668fcf0d14d9385a004dae5066e9d019070
Jinkeyu <37464959+Jinkeyu@users.noreply.github.com>
2018-04-20 10:16:03 +0800

    1

--- calobjValue.py
+++ calobjValue.py
@@ -3,7 +3,7 @@
 
 import math
 
-#针对整个种群的解码函数
+#Decode function of whole population(针对整个种群的解码函数)
 def decodechrom(Pop, chrom_length):
     temp = []
     for i in range(len(Pop)):
@@ -15,20 +15,20 @@ def decodechrom(Pop, chrom_length):
             temp[i].append(t)
     return temp
 
-#计算开始时间的函数
-#Pop: 经过解码之后的种群
-#Chrom_length: 基因长度
-#Timelist    : 供电时间表
+#Function to calculate the start time                        计算开始时间的函数
+#Pop: The population after the decoding (经过解码之后的种群)
+#Chrom_length: gene length    (基因长度)
+#Timelist    : working time   (供电时间表)
 def caltimeValue(Pop_decoded,Chrom_length,Timelist):
     Variable = 0 
-    Temp_pop_start = []                            #用来记录电器开始工作的时间
-    Temp_pop_end = []                              #用来记录电器结束工作的时间
+    Temp_pop_start = []                            #recording the starting time of appliances用来记录电器开始工作的时间
+    Temp_pop_end = []                              #recording the ending time of appliances用来记录电器结束工作的时间
     for i in range(len(Timelist)):
         for j in range(len(Timelist[i])//2):
             Starttime = Timelist[i][2*j]           #获得每个用电器的使用开始和结束时间，由于时间不一定是一段，所以要分情况讨论
             Endtime = Timelist[i][2*j+1]
             if Starttime < Endtime:
-                Period = Endtime - Starttime       #计算每个用电器的使用时长
+                Period = Endtime - Starttime       #The working period of every appliance计算每个用电器的使用时长
             else:
                 Period = (24 - Starttime + Endtime)
             
@@ -46,7 +46,7 @@ def caltimeValue(Pop_decoded,Chrom_length,Timelist):
                 
                 temp = temp + Period      #计算用电器结束使用的时间
                 if temp > 24.0:
-                    temp = temp - 24.0    #调整时间范围，确保时间段落入0——24小时中
+                    temp = temp - 24.0    #Adjust the range of time to ensure it is located in 0-24 hrs调整时间范围，确保时间段落入0——24小时中
                 elif temp < 0:
                     temp = -temp 
                 Temp_pop_end[Variable].append(temp)
@@ -59,31 +59,31 @@ def caltimeValue(Pop_decoded,Chrom_length,Timelist):
 #解码并计算开始结束时间
 def calobjValue(Pop, Chrom_length, Timelist, Species_size):
     Pop_decoded = []                     
-    Pop_decoded = decodechrom(Pop, Chrom_length)   #经过解码的种群
+    Pop_decoded = decodechrom(Pop, Chrom_length)   #The population after decoding (经过解码的种群)
     Temp_pop_start,Temp_pop_end = caltimeValue(Pop_decoded, Chrom_length, Timelist)
     return Temp_pop_start, Temp_pop_end
 
 
-#计算用户电费的程序
+#calculate the user's cost    (计算用户电费的程序)
 def calculateCost(User1):
-    Peak_starttime = 6.0       #峰电开始时间
-    Peak_endtime = 22.0        #峰电结束时间
-    Peak_period = 16.0         #峰电持续时间(Peak_endtime - Peak_starttime)
+    Peak_starttime = 6.0       #Peak period starting time   峰电开始时间
+    Peak_endtime = 22.0        #Peak period ending time   峰电结束时间
+    Peak_period = 16.0         #Time period of peak period   峰电持续时间(Peak_endtime - Peak_starttime)
     
-    Valley = 0.6               #谷电电价
-    Peak = 1.2                 #峰电电价    
+    Valley = 0.6               #valley price   谷电电价
+    Peak = 1.2                 #Peak price   峰电电价    
     
-    Cost = 0                   #总电费
-    Time_slots = 0             #总使用电器时间段数目
-    Timelist = []              #开始时间表
-    Individual_cost = []       #单项电费表
+    Cost = 0                   #Total cost总电费
+    Time_slots = 0             #Total time slots of all appliances   总使用电器时间段数目
+    Timelist = []              #Start timelist   开始时间表
+    Individual_cost = []       #cost list of Single item   单项电费表
     
     for i in range((len(User1)//2)):    
-        Temp = User1[2*i+1]        #存储电器的使用时间的列表
-        Power = User1[2*i][0]         #存储当前使用电器功率值
+        Temp = User1[2*i+1]        #Storage the list of appliance' working period   存储电器的使用时间的列表
+        Power = User1[2*i][0]         #Storage the current appliance power value   存储当前使用电器功率值
         Timelist.append(User1[2*i+1][0])
         #print(Temp)
-        #考虑所有的情况，一共有12种电费计价方式
+        #Considering all circumstances, there are totally 12 ways to calculate the electricity cost  s考虑所有的情况，一共有12种电费计价方式
         j = 0
         for j in range(len(Temp[0])//2):
             Starttime = Temp[0][2*j]
@@ -121,7 +121,7 @@ def calculateCost(User1):
                     else:
                         Cost = Cost + (24.0 - Peak_endtime + Peak_starttime)*Valley*Power + (Peak_endtime - Peak_starttime - (Starttime - Endtime))*Peak*Power
              
-            #情况四，开始时间大于峰值结束时间
+            #Case 4, Starting time is larger than the end of end time情况四，开始时间大于峰值结束时间
             elif (Starttime >= Peak_endtime and Endtime <= Peak_endtime):
                 if(Endtime < Peak_starttime):
                     Cost = Cost + (24.0 - Starttime + Endtime)*Valley*Power
@@ -131,14 +131,14 @@ def calculateCost(User1):
             Individual_cost.append(Cost - Last_cost)
     return Cost , Time_slots , Timelist , Individual_cost
 
-#得到时间表之后，计算每一种情况电费的程序
+#Get the timelist, calculate the cost of every condition得到时间表之后，计算每一种情况电费的程序
 def calcostValue(Temp_pop_start,Temp_pop_end,User):
-    Cost_list = []             #电费列表
+    Cost_list = []             #electricity price list电费列表
     for i in range(len(Temp_pop_start)):
         Cost_list.append([])
 
     for k in range(len(Temp_pop_start[0])):
-        Variable = 0               #电器使用时间段数
+        Variable = 0               #The working time slots of appliance电器使用时间段数
         for i in range(len(User)//2):
             for j in range(len(User[2*i + 1][0])//2):        
                 User[2*i+1][0][2*j] = Temp_pop_start[Variable][k]
